102b2fc6bf2625fc7b59f084565d4f6aba542235
Rohit kumar yadav <76595116+Rohit-kumar-yadav@users.noreply.github.com>
2024-07-08 00:28:05 +0530

    intro

--- intro/src/App.jsx
+++ intro/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <>
       <h1>Welcome To React Series</h1>
+      <p>React is the library for web and native user interfaces. Build user interfaces out of individual pieces called components written in JavaScript.</p>
     </>
   )
 }
